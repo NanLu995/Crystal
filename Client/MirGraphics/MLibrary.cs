1bffd503a31918daf2930c19ea8abd3bf1e8e3b7
lancetti928 <lancetti928@qq.com>
2026-01-12 11:44:09 +0800

    【预添加】客户端MLibrary部分新增代码，暂时屏蔽，后期研究

--- Client/MirGraphics/MLibrary.cs
+++ Client/MirGraphics/MLibrary.cs
@@ -18,6 +18,7 @@ namespace Client.MirGraphics
             Prguse2 = new MLibrary(Settings.DataPath + "Prguse2"),
             Prguse3 = new MLibrary(Settings.DataPath + "Prguse3"),
             UI_32bit = new MLibrary(Settings.DataPath + "UI_32bit"),
+            //StateitemEffect = new MLibrary(Settings.DataPath + "StateitemEffect"),
             BuffIcon = new MLibrary(Settings.DataPath + "BuffIcon"),
             Help = new MLibrary(Settings.DataPath + "Help"),
             MiniMap = new MLibrary(Settings.DataPath + "MMap"),
@@ -28,7 +29,10 @@ namespace Client.MirGraphics
             Magic = new MLibrary(Settings.DataPath + "Magic"),
             Magic2 = new MLibrary(Settings.DataPath + "Magic2"),
             Magic3 = new MLibrary(Settings.DataPath + "Magic3"),
+            //Magic_32bit = new MLibrary(Settings.DataPath + "Magic_32bit"),
             Effect = new MLibrary(Settings.DataPath + "Effect"),
+            //Effect2 = new MLibrary(Settings.DataPath + "Effect2"),
+            //Effect_32bit = new MLibrary(Settings.DataPath + "Effect_32bit"),
             MagicC = new MLibrary(Settings.DataPath + "MagicC"),
             GuildSkill = new MLibrary(Settings.DataPath + "GuildSkill"),
             Weather = new MLibrary(Settings.DataPath + "Weather");
@@ -62,9 +66,13 @@ namespace Client.MirGraphics
                                           AArmours,
                                           AWeaponsL,
                                           AWeaponsR,
+                                          //AWeaponEffectL,
+                                          //AWeaponEffectR,
                                           AHair,
                                           AHumEffect,
                                           ARArmours,
+                                          //ARWeaponsEffect,
+                                          //ARWeaponsEffectS,
                                           ARWeapons,
                                           ARWeaponsS,
                                           ARHair,
@@ -96,6 +104,8 @@ namespace Client.MirGraphics
             InitLibrary(ref AHair, Settings.AHairPath, "00");
             InitLibrary(ref AWeaponsL, Settings.AWeaponPath, "00", " L");
             InitLibrary(ref AWeaponsR, Settings.AWeaponPath, "00", " R");
+            //InitLibrary(ref AWeaponEffectL, Settings.AWeaponEffectPath, "00", " L");
+            //InitLibrary(ref AWeaponEffectR, Settings.AWeaponEffectPath, "00", " R");
             InitLibrary(ref AHumEffect, Settings.AHumEffectPath, "00");
 
             //Archer
@@ -103,6 +113,8 @@ namespace Client.MirGraphics
             InitLibrary(ref ARHair, Settings.ARHairPath, "00");
             InitLibrary(ref ARWeapons, Settings.ARWeaponPath, "00");
             InitLibrary(ref ARWeaponsS, Settings.ARWeaponPath, "00", " S");
+            //InitLibrary(ref ARWeaponsEffect, Settings.ARWeaponEffectPath, "00");
+            //InitLibrary(ref ARWeaponsEffectS, Settings.ARWeaponEffectPath, "00", " S");
             InitLibrary(ref ARHumEffect, Settings.ARHumEffectPath, "00");
 
             //Other
@@ -124,6 +136,7 @@ namespace Client.MirGraphics
             MapLibs[0] = new MLibrary(Settings.DataPath + "Map\\WemadeMir2\\Tiles");
             MapLibs[1] = new MLibrary(Settings.DataPath + "Map\\WemadeMir2\\Smtiles");
             MapLibs[2] = new MLibrary(Settings.DataPath + "Map\\WemadeMir2\\Objects");
+            //for (int i = 2; i < 29; i++)
             for (int i = 2; i < 28; i++)
             {
                 MapLibs[i + 1] = new MLibrary(Settings.DataPath + "Map\\WemadeMir2\\Objects" + i.ToString());
@@ -239,9 +252,11 @@ namespace Client.MirGraphics
         {
             Count = MapLibs.Length + Monsters.Length + Gates.Length + Flags.Length + Siege.Length + NPCs.Length + CArmours.Length +
                 CHair.Length + CWeapons.Length + CWeaponEffect.Length + AArmours.Length + AHair.Length + AWeaponsL.Length + AWeaponsR.Length +
+                //AWeaponEffectL.Length + AWeaponEffectR.Length +ARArmours.Length + ARHair.Length + ARWeapons.Length + ARWeaponsS.Length + ARWeaponsEffect.Length + ARWeaponsEffectS.Length +
                 ARArmours.Length + ARHair.Length + ARWeapons.Length + ARWeaponsS.Length +
                 CHumEffect.Length + AHumEffect.Length + ARHumEffect.Length + Mounts.Length + Fishing.Length + Pets.Length +
-                Transform.Length + TransformMounts.Length + TransformEffect.Length + TransformWeaponEffect.Length + 19;
+                //Transform.Length + TransformMounts.Length + TransformEffect.Length + TransformWeaponEffect.Length + 18;
+				Transform.Length + TransformMounts.Length + TransformEffect.Length + TransformWeaponEffect.Length + 19;
 
             Dragon.Initialize();
             Progress++;
@@ -268,10 +283,16 @@ namespace Client.MirGraphics
             Progress++;
             Magic3.Initialize();
             Progress++;
+            //Magic_32bit.Initialize();
+            //Progress++;
             MagicC.Initialize();
             Progress++;
 
             Effect.Initialize();
+            //Progress++;
+            //Effect2.Initialize();
+            //Progress++;
+            //Effect_32bit.Initialize();
             Progress++;
 
             Weather.Initialize();
@@ -383,6 +404,18 @@ namespace Client.MirGraphics
                 Progress++;
             }
 
+            /**for (int i = 0; i < AWeaponEffectL.Length; i++)
+            {
+                AWeaponEffectL[i].Initialize();
+                Progress++;
+            }
+
+            for (int i = 0; i < AWeaponEffectR.Length; i++)
+            {
+                AWeaponEffectR[i].Initialize();
+                Progress++;
+            }*/
+
             for (int i = 0; i < ARArmours.Length; i++)
             {
                 ARArmours[i].Initialize();
@@ -406,6 +439,18 @@ namespace Client.MirGraphics
                 ARWeaponsS[i].Initialize();
                 Progress++;
             }
+
+            /**for (int i = 0; i < ARWeaponsEffect.Length; i++)
+            {
+                ARWeaponsEffect[i].Initialize();
+                Progress++;
+            }
+
+            for (int i = 0; i < ARWeaponsEffectS.Length; i++)
+            {
+                ARWeaponsEffectS[i].Initialize();
+                Progress++;
+            }*/
 
             for (int i = 0; i < CHumEffect.Length; i++)
             {
